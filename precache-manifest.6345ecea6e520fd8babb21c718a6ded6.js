3a77d93e7f3450069f32536fa6043b17b481db05
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-04-19 12:26:41 -0300

    Updates

---
+++ precache-manifest.6345ecea6e520fd8babb21c718a6ded6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7cae702ad2cdf1a5a1c153c7b634ef",
+    "url": "/simple-dnd/index.html"
+  },
+  {
+    "revision": "6b458a49d8f4d8ed79b8",
+    "url": "/simple-dnd/static/css/main.bf03791b.chunk.css"
+  },
+  {
+    "revision": "f29c2800c28bd1ca582c",
+    "url": "/simple-dnd/static/js/2.d9adb09e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-dnd/static/js/2.d9adb09e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b458a49d8f4d8ed79b8",
+    "url": "/simple-dnd/static/js/main.c7209a5e.chunk.js"
+  },
+  {
+    "revision": "4e05e1e690a77bacb694",
+    "url": "/simple-dnd/static/js/runtime-main.4c3b387d.js"
+  }
+]);
